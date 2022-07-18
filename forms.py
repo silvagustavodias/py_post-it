99758640ccd21184d578817516524e6b076b9002
Gustavo Dias <silvagustavodias@gmail.com>
2022-07-18 18:19:45 -0300

    Configurado formulário login

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormCriarConta(FlaskForm):
@@ -14,4 +14,6 @@ class FormCriarConta(FlaskForm):
 class FormLogin(FlaskForm):
     email = StringField('E-mail', validators=[DataRequired(), Email()])
     senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     botao_submit_login = SubmitField('Fazer Login')
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2c3e8d6ff5cc32e8a9d3b6b25ad4714f'
 
 
 list_users = ['Gustavo', 'Débora', 'Pedro', 'Giselly']
@@ -19,9 +21,11 @@ def users():
     #Passando a variável para o código html
     return render_template('users.html', list_users=list_users)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form_login = FormLogin()
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login = form_login, form_criarconta = form_criarconta)
 
 if __name__ == '__main__':
     # valor para acatar mudanças automaticamente
