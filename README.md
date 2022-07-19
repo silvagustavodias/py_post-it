865daf1686b156f6152077ba7fec7407472eeb96
Gustavo Dias <silvagustavodias@gmail.com>
2022-07-19 01:04:08 -0300

    Realizado primeiras configurações do Banco de Dados.

--- README.md
+++ README.md
@@ -14,3 +14,37 @@ Site para compartilhar notícias e opiniões e testar a utilização de Flask no
 
 **Back-end:** Flask
 
+------
+## Documentation
+Para fins de estudos posteriores.
+
+### main.py
+#### imports realizados
+* Flask: classe para inciar o app. importa-se Flask(__name__)
+* render_template: para renderizar o html vindo da pasta template
+* url_for: útil para tornar o endereço contido nos decorator uma variável
+* request: para realizar requisições no site. Neste caso concreto foi utilizado para auxiliar na validação dos botões
+* flash: mensagens de alertas do Flask
+* redirect: método para redirecionar.
+
+#### functions
+As páginas são definidas por funções:
+
+Ex:
+@app.route('/contatos')
+def contato():
+    function....
+
+A função def login() contém uma lógica de validação nos botões e no método já pronto de validação de formulário.
+
+
+### models.py
+
+
+### forms.py
+
+
+### templates
+
+
+
---
+++ models.py
@@ -0,0 +1,20 @@
+from main import database
+from datetime import datetime
+
+
+class Usuario(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String, nullable=False)
+    senha = database.Column(database.String, nullable=False)
+    email = database.Column(database.String, nullable=False, unique=True)
+    foto_perfil = database.Column(database.String, default='padrao.jpg')
+    posts = database.relationship('Post', backref='author', lazy=True)
+    interesses = database.Column(database.String, nullable=False, default='Não informado')
+
+
+class Post(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    titulo = database.Column(database.String, nullable=False)
+    corpo = database.Column(database.Text, nullable=False)
+    data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow())
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
